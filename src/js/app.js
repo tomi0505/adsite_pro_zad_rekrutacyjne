da3501c403ff4c75e290708dc536cf9b0f96278f
tomi0505 <tgdevit@gmail.com>
2024-09-07 10:34:25 +0200

    slider

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,25 @@ document.addEventListener('DOMContentLoaded', () => {
     mobileMenu.classList.toggle('hidden');
   });
 
-  $( function() {
-    $( "#tabs" ).tabs();
-  } );
+  $(function () {
+    $("#tabs").tabs();
+  });
+
+  $('.slider-1').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    mobileFirst: true,
+    autoplay: true,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
 });
